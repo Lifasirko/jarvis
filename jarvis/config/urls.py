548d6e8ba10904ace477fb24eb2dabcb6413a334
Amolit <MikeKukPer@gmail.com>
2024-07-20 16:17:02 +0200

    Added home page, Log In, Sign Up, Log Out

--- jarvis/config/urls.py
+++ jarvis/config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
 
 ]
