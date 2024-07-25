0264684064f5dfe6581fac0e8cebf6a26c72ba6b
paulmusquaro <136118097+paulmusquaro@users.noreply.github.com>
2024-07-25 20:31:52 +0300

    Commit Four

--- jarvis/core/urls.py
+++ jarvis/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import home_view, register_view, login_view, logout_view, contact_list_view, \
-    file_list_view, news_view, note_list_view
+    file_list_view, news_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -10,7 +10,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     # path('dashboard/', dashboard_view, name='dashboard'),
     path('contacts/', contact_list_view, name='contact_list'),
-    # path('notes/', note_list_view, name='note_list'),
     path('files/', file_list_view, name='file_list'),
     path('news/', news_view, name='news'),
 ]
