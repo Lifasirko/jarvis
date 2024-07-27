d2442036a7906f5949e90be5e8235818e5c01726
BrainFisher <164922601+BrainFisher@users.noreply.github.com>
2024-07-27 09:56:28 +0300

    Add files via upload

--- jarvis/config/urls.py
+++ jarvis/config/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
                   path('notes/', include('notes.urls')),
                   path('accounts/', include('allauth.urls')),
                   path('tasks/', include('task_manager.urls')),
+                  
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
