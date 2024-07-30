a03e154ee498e83e99afa5d814bec26f07fb5c42
Amolit <MikeKukPer@gmail.com>
2024-07-31 01:25:30 +0200

    Partly marge with brain2

--- jarvis/config/settings.py
+++ jarvis/config/settings.py
@@ -98,6 +98,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'core.context_processors.avatar_processor',
+
             ],
         },
     },
